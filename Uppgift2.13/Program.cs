a9839b895ad3401b158671a41147abf8363f4f4f
Dungeater1337 <111962656+Dungeater1337@users.noreply.github.com>
2022-09-14 17:01:20 +0200

    Add project files.

---
+++ Uppgift2.13/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+    namespace uppgift2_13
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vill du ge ut lönerna till lönetagarna?");
+            Console.ReadLine();
+
+            Console.WriteLine("Ja? Lägg ihop teras löner.");
+            Console.ReadLine();
+
+            Console.WriteLine("Hur mycket lön har ska Thomas få?");
+            string strThomaslöntext = Console.ReadLine();
+            int Thomaslön = int.Parse(strThomaslöntext);
+
+            Console.WriteLine("Hur mycket lön ska Jacob få?");
+            string strJacobslöntext = Console.ReadLine();
+            int Jacobslön = int.Parse(strJacobslöntext);
+
+            Console.WriteLine("hur mycket lön ska David få?");
+            string strDavidslöntext = Console.ReadLine();
+            int Davidslön = int.Parse(strDavidslöntext);
+
+            int summan;
+
+            summan = Thomaslön + Jacobslön + Davidslön;
+            Console.WriteLine("Deras löner sammanlagt blir " + summan); 
+
+
+        }
+    }
+}
+
